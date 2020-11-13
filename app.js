524b593b138bbb5d4ec540bce487770269fab3c5
firman Y <fy@gmail.com>
2020-11-13 15:30:10 -0500

    Add If statement/function

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const x = "SPANKED THE"
-console.log(x)
+// const x = "SPANKED THE"
+// console.log(x)
+let apples = 10
+let oranges = 5
+let x;
+if(apples > oranges){
+    x = apples - oranges
+    console.log(`Firman has ${x} more apples than oranges`)    
+}   else {
+    console.log( "I have more oranges than apples")
+}
+
+const juiceMaker = (apples,oranges) => apples * oranges  
+const juiceMaker = (apples,oranges) => {
+    return apples * oranges
+} 
+juiceMaker( 3,4)
+console.log(juice(3,4)) 
